55901af7b63290d18406d03380691a3e598b2723
James Whiteneck <james.whiteneck@rentrak.com>
2013-12-12 09:28:50 -0800

    Removes tests, installation files, and change logs.

--- test/before_script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# If were are not on the bleeding edge git
-if [[ "$GIT_PPA" != "ppa:pdoes/ppa" ]]; then
-  # then use the Ubuntu one
-  sudo apt-get install ppa-purge -y
-  sudo ppa-purge "ppa:pdoes/ppa" -y
-fi
